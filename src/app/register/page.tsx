3ea6b2bc24b3ccd2c92093792282bde1b4d4246e
Tran Huy Hoang <huyhoangero@gmail.com>
2025-04-30 23:19:30 +0700

    Update MVP 3

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -214,7 +214,6 @@ export default function Register() {
                 >
                   <option value="PATIENT">Patient</option>
                   <option value="DOCTOR">Doctor</option>
-                  <option value="ADMIN">Admin</option>
                 </select>
               </div>
 
